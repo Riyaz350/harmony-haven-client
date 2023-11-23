adf0f5331b9f84bdf8c0f2343e46f5c8694c4c3c
riyaz <riyaz3582@gmail.com>
2023-11-23 20:45:29 +0600

    created client side installed axios, react-router, daisyUI, tailwind, firebase and created routes, and several empty components

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+    return (
+        <div>
+            <h1>This is main</h1>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,67 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth"
+import auth from "../../firebase.config"
+import { createContext, useEffect, useState } from "react"
+import axios from "axios"
+
+export const AuthContext = createContext(null)
+const AuthProvider = ({children}) => {
+    const [loading, setLoading] =useState(true)
+    const [user, setUser] =useState(null)
+
+
+    const createUser = ( email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInUser = (email, password) =>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInPop = (provider) =>{
+        setLoading(true)
+        return signInWithPopup(auth, provider)
+    }
+
+    const logOut = () =>{
+        return signOut(auth)
+    }
+
+
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+            const userEmail = currentUser?.email || user?.email 
+            const loggedUser = {email: userEmail}
+            setUser(currentUser)
+            setLoading(false)
+            if(currentUser){
+                axios.post('https://assignment-server-sand.vercel.app/jwt', loggedUser, {withCredentials: true})
+                .then()
+            }
+            else{
+                // console.log('logged out')
+                axios.post('https://assignment-server-sand.vercel.app/logout', loggedUser, {withCredentials: true})
+                .then(res => {
+                    console.log(res.data)
+                })
+            }
+
+
+        })
+        return()=>{
+          unSubscribe()  
+        }
+    },[user?.email])
+
+
+    const authInfo = { user,loading, createUser, signInUser,signInPop, logOut }
+
+return(
+<AuthContext.Provider value={authInfo}>
+    {children}
+</AuthContext.Provider>
+)
+}
+
+export default AuthProvider;
