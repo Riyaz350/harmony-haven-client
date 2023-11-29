0f54c7c9b11600fe57f99c1839f69371a781ea0b
riyaz <riyaz3582@gmail.com>
2023-11-29 08:28:52 +0600

    added modal option to make coupons page

--- src/Layout/DashBoard/DashboardItems/Admin/ManageCoupons/ManageCoupons.jsx
+++ src/Layout/DashBoard/DashboardItems/Admin/ManageCoupons/ManageCoupons.jsx
@@ -4,6 +4,7 @@ import useCouponInfo from "../../../../../Hooks/useCouponInfo";
 import CouponCard from "../../../../../Pages/Home/HomeComponents/Coupon/CouponCard";
 import ManageCouponCard from "./ManageCouponCard";
 import SubTitle from "../../../../../Hooks/SubTitle";
+import '../../../../../App.css'
 
 const ManageCoupons = () => {
     const  [coupons, couponLoading] = useCouponInfo()
@@ -32,6 +33,8 @@ const ManageCoupons = () => {
 <div className="lg:min-h-screen">
             <SubTitle title='Manage Coupons'></SubTitle>
 
+            
+
             <div className="overflow-x-auto max-w-7xl mx-auto my-5 ">
              <table className="table w-full">
                 {/* head */}
@@ -56,7 +59,40 @@ const ManageCoupons = () => {
             </table>
             </div>    
 
-        
+
+            <div className="flex justify-center">
+            <button className="btn btnLandLord text-white " onClick={()=>document.getElementById('my_modal_2').showModal()}>Create Coupon</button>
+                <dialog id="my_modal_2" className="modal">
+                <div className="flex flex-col items-center justify-center ">
+
+                <div className="flex flex-col  gap-10 justify-center items-start bg-[#00a9a5]  p-10 md:p-20">
+                <h1 className="text-white font-bold  text-center bg-[#00a9a5] p-6 rounded-full ">Make A Coupon</h1>
+                <form onSubmit={handleCoupons} className='space-y-4'>
+                    <h1 className="text-red-500 font-bold text-lg">Write the percentage of discount in front of the coupon like this =10coupon</h1>
+                    <input name="title" type="text" className="border-b-2 border-black w-fit h-20" placeholder="Example '10coupon'" />
+                    <textarea name="message" placeholder="Describe the coupon" className="textarea textarea-bordered textarea-lg w-full max-w-lg" ></textarea>
+                    <button className='btnLandLord btn text-white'>Publish</button>
+                </form>
+                </div>
+            </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+            </div>
+
+            {/* <div className="flex flex-col items-center justify-center ">
+
+                <div className="flex flex-col w-1/2 gap-10 justify-center items-start">
+                <h1 className="text-white font-bold  text-center bg-[#00a9a5] p-6 rounded-full ">Make A Coupon</h1>
+                <form onSubmit={handleCoupons} className='space-y-4'>
+                    <h1 className="text-red-500 font-bold text-lg">Write the percentage of discount in front of the coupon like this =10coupon</h1>
+                    <input name="title" type="text" className="border-b-2 border-black w-fit h-20" placeholder="Example '10coupon'" />
+                    <textarea name="message" placeholder="Describe the coupon" className="textarea textarea-bordered textarea-lg w-full max-w-lg" ></textarea>
+                    <button className='btnLandLord btn text-white'>Publish</button>
+                </form>
+                </div>
+            </div> */}
            
             
             </div>
@@ -72,15 +108,15 @@ const ManageCoupons = () => {
 
 
     //    <div className="flex flex-col items-center justify-center ">
-    //         <div className="flex flex-col w-1/2 gap-10 justify-center items-start">
-    //         <h1 className="text-white font-bold  text-center bg-[#00a9a5] p-6 rounded-full ">Make A Coupon</h1>
-    //         <form onSubmit={handleCoupons} className='space-y-4'>
-    //             <h1 className="text-red-500 font-bold text-lg">Write the percentage of discount in front of the coupon like this =10coupon</h1>
-    //             <input name="title" type="text" className="border-b-2 border-black w-fit h-20" placeholder="Example '10coupon'" />
-    //             <textarea name="message" placeholder="Describe the coupon" className="textarea textarea-bordered textarea-lg w-full max-w-lg" ></textarea>
-    //             <button className='btnLandLord btn text-white'>Publish</button>
-    //         </form>
-    //         </div>
+            // <div className="flex flex-col w-1/2 gap-10 justify-center items-start">
+            // <h1 className="text-white font-bold  text-center bg-[#00a9a5] p-6 rounded-full ">Make A Coupon</h1>
+            // <form onSubmit={handleCoupons} className='space-y-4'>
+            //     <h1 className="text-red-500 font-bold text-lg">Write the percentage of discount in front of the coupon like this =10coupon</h1>
+            //     <input name="title" type="text" className="border-b-2 border-black w-fit h-20" placeholder="Example '10coupon'" />
+            //     <textarea name="message" placeholder="Describe the coupon" className="textarea textarea-bordered textarea-lg w-full max-w-lg" ></textarea>
+            //     <button className='btnLandLord btn text-white'>Publish</button>
+            // </form>
+            // </div>
 
     //         <div className="md:grid grid-cols-3 max-w-6xl gap-6 space-y-5 md:space-y-0">
                 // {
