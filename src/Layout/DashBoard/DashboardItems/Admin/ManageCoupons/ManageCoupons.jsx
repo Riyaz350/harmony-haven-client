d933abde2643901cb1196360d3a090f53f4ddaac
riyaz <riyaz3582@gmail.com>
2023-11-30 11:08:01 +0600

    added dashboard redirection to homepage, spinner to manage coupons page

--- src/Layout/DashBoard/DashboardItems/Admin/ManageCoupons/ManageCoupons.jsx
+++ src/Layout/DashBoard/DashboardItems/Admin/ManageCoupons/ManageCoupons.jsx
@@ -7,7 +7,7 @@ import SubTitle from "../../../../../Hooks/SubTitle";
 import '../../../../../App.css'
 
 const ManageCoupons = () => {
-    const  [coupons, couponLoading] = useCouponInfo()
+    const  [coupons, couponLoading, refetch] = useCouponInfo()
     const axiosSecure = useAxiosSecure()
 
     const handleCoupons = e =>{
@@ -21,6 +21,7 @@ const ManageCoupons = () => {
         .then(res=>{
             if(res.status == 200){
                 Swal.fire({position: "top-end", icon: "success", title: "Coupon Published", showConfirmButton: false, timer: 1500});
+                refetch()
                 e.target.reset()
             }
         })
@@ -33,7 +34,7 @@ const ManageCoupons = () => {
 <div className="lg:min-h-screen ">
             <SubTitle title='Manage Coupons'></SubTitle>
 
-            
+            {couponLoading && <span className="loading w-1/6 mx-auto py-80 text-center block loading-spinner text-[#00a9a5]"></span>}
 
             <div className="overflow-x-auto max-w-7xl mx-auto my-5 ">
              <table className="table w-full  static">
@@ -81,18 +82,6 @@ const ManageCoupons = () => {
             </dialog>
             </div>
 
-            {/* <div className="flex flex-col items-center justify-center ">
-
-                <div className="flex flex-col w-1/2 gap-10 justify-center items-start">
-                <h1 className="text-white font-bold  text-center bg-[#00a9a5] p-6 rounded-full ">Make A Coupon</h1>
-                <form onSubmit={handleCoupons} className='space-y-4'>
-                    <h1 className="text-red-500 font-bold text-lg">Write the percentage of discount in front of the coupon like this =10coupon</h1>
-                    <input name="title" type="text" className="border-b-2 border-black w-fit h-20" placeholder="Example '10coupon'" />
-                    <textarea name="message" placeholder="Describe the coupon" className="textarea textarea-bordered textarea-lg w-full max-w-lg" ></textarea>
-                    <button className='btnLandLord btn text-white'>Publish</button>
-                </form>
-                </div>
-            </div> */}
            
             
             </div>
